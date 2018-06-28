bfa52835b92f6010182ace5f8051930b3c7dac57
Jakub Martin <jakub.wit.martin@gmail.com>
2018-06-29 01:16:11 +0200

    Testing the notifier

--- notifier/cmd/main.go
+++ notifier/cmd/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/cube2222/usos-notifier/notifier/service"
 	"github.com/gorilla/mux"
-	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -17,6 +16,5 @@ func main() {
 
 	m := mux.NewRouter()
 	m.HandleFunc("/webhook", s.HandleWebhookHTTP())
-	http.ListenAndServe(":80", m)
-	log.Fatal(http.Serve(autocert.NewListener("notifier.jacobmartins.com"), m))
+	http.ListenAndServeTLS(":443", "cert.crt", "cert.key", m)
 }
