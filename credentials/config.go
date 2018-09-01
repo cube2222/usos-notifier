22d9fa925c4ec92fb02a45cea62f3be37697d32f
Jakub Martin <jakub.wit.martin@gmail.com>
2018-09-01 15:16:45 +0200

    Credentials config

---
+++ credentials/config.go
@@ -0,0 +1,13 @@
+package credentials
+
+type Config struct {
+	ListenPortHttp int `default:"8080" split_words:"true"`
+	ListenPortGrpc int `default:"8081" split_words:"true"`
+
+	ProjectName                  string `default:"usos-notifier" split_words:"true"`
+	AdditionalAuthenticatedData  string `default:"something" split_word:"true"`
+	EncryptionKeyID              string `default:"projects/usos-notifier/locations/global/keyRings/credentials/cryptoKeys/credentials" split_word:"true"`
+	NotificationsTopic           string `default:"notifications" split_words:"true"`
+	UserCreatedSubscription      string `default:"credentials-notifier-user_created" split_words:"true"`
+	GoogleApplicationCredentials string `default:"/var/secrets/google/serviceaccount.json" split_words:"true"`
+}
