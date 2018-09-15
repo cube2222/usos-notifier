0a80e42abdd410bfc16ecddf7120b984c986a1a2
Jakub Martin <jakub.wit.martin@gmail.com>
2018-09-15 23:52:54 +0200

    Cleanup

--- notifier/sender_test.go
+++ notifier/sender_test.go
@@ -1,23 +1,15 @@
 package notifier
 
-import (
-	"context"
-	"testing"
-
-	"cloud.google.com/go/pubsub"
-	"google.golang.org/api/option"
-)
-
-func TestNotificationSender_SendNotification(t *testing.T) {
+/*func TestNotificationSender_SendNotification(t *testing.T) {
 	cli, err := pubsub.NewClient(context.Background(), "usos-notifier", option.WithCredentialsFile("C:/Development/Projects/Go/src/github.com/cube2222/usos-notifier/usos-notifier-9a2e44d7f26b.json"))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	sender := NewNotificationSender(cli)
+	sender := NewNotificationSender(cli, "test")
 
 	err = sender.SendNotification(context.Background(), "me", "New method!")
 	if err != nil {
 		t.Fatal(err)
 	}
-}
+}*/
